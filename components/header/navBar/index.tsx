918d374c9f7ca2cf5c506e1b4d48b064ab912c9f
Van Huy <vanhuy@Hy-MacBook.local>
2022-02-07 17:35:09 +0700

    bk nav menu

--- components/header/navBar/index.tsx
+++ components/header/navBar/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface INavBarProps {
     open: boolean
 }
@@ -7,10 +9,18 @@ const NavBar = ({ open }: INavBarProps) => {
         <div className={open ? "navigate open" : "navigate"}>
             <div className="container">
                 <div className="navigate__wrapper">
-                    <div className="navigate__menu">
-                        sdnasjdasdahsdjasd
-                    </div>
-                    <div className="navigate__social">
+                    <nav className="navigate__wrapper-nav">
+                        <Link href="/abc">
+                            <a>Experience</a>
+                        </Link>
+                        <Link href="/def">
+                            <a>Project</a>
+                        </Link>
+                        <Link href="/ghi">
+                            <a>Contact</a>
+                        </Link>
+                    </nav>
+                    <div className="navigate__wrapper-social">
                         asjdaksbdkasbdkasb
                     </div>
                 </div>
